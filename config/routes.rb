c5b846816040c6de61d7a25b79e70744c28082e9
FelixPriori <felixriouxsabourin@gmail.com>
2020-04-01 20:08:52 +0000

    users can register, logout, and login

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
-  resources :about, only: [:show]
+  get '/about' => 'about#show'
 
   resources :orders, only: [:create, :show]
 
@@ -19,6 +19,15 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
   end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/register' => 'users#new'
+  post '/users' => 'users#create'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

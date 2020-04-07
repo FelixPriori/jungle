7af856c374e37ac1798bd382dc97f50a82ef74d8
FelixPriori <felixriouxsabourin@gmail.com>
2020-04-07 13:40:01 +0000

    Adds password_confirmation field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if @user.save
+    if user_params[:password] == user_params[:password_confirmation] && @user.save
       session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/new'
+      redirect_to '/register'
     end
   end
 

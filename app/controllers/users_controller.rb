09ca37b8b6876b1d9c7e7914333ac5e4488c0510
FelixPriori <felixriouxsabourin@gmail.com>
2020-04-07 18:50:01 +0000

    implments tests for password_confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if user_params[:password] == user_params[:password_confirmation] && @user.save
+    if @user.save
       session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/register'
+      render 'new'
     end
   end
 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,65 +4,72 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
 
-    # TODO : add password_confirmation
     context 'password do not match' do
       it 'should not save successfully' do
         @user = User.create(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password', password_confirmation: 'something else')
+        @user.save
         expect(@user).to_not be_valid
       end
     end
 
     context 'with password, email, first_name, last_name' do
       it 'should save successfully' do
-        @user = User.new(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password')
+        @user = User.new(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password', password_confirmation: 'password')
+        @user.save
         expect(@user).to be_valid
       end
     end
 
     context 'with duplicated email' do
       it 'should not save successfully' do
-        @user1 = User.create(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password')
+        @user1 = User.create!(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password', password_confirmation: 'password')
         @user2 = User.new(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password')
+        @user2.save
         expect(@user2).to_not be_valid
       end
       it 'should not be case sensitive' do
-        @user1 = User.create(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password')
-        @user2 = User.new(first_name: 'bob', last_name: 'bobby', email: 'BOB@EXAMPLE.COM', password: 'password')
+        @user1 = User.create!(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password', password_confirmation: 'password')
+        @user2 = User.new(first_name: 'bob', last_name: 'bobby', email: 'BOB@EXAMPLE.COM', password: 'password', password_confirmation: 'password')
+        @user2.save
         expect(@user2).to_not be_valid
       end
     end
 
     context 'without an email' do
       it 'should not save successfully' do
-        @user = User.new(first_name: 'bob', last_name: 'bobby', email: nil, password: 'password')
+        @user = User.new(first_name: 'bob', last_name: 'bobby', email: nil, password: 'password', password_confirmation: 'password')
+        @user.save
         expect(@user).to_not be_valid
       end
     end
 
     context 'without a last_name' do
       it 'should not save successfully' do
-        @user = User.new(first_name: 'bob', last_name: nil, email: 'bob@example.com', password: 'password')
+        @user = User.new(first_name: 'bob', last_name: nil, email: 'bob@example.com', password: 'password', password_confirmation: 'password')
+        @user.save
         expect(@user).to_not be_valid
       end
     end
 
     context 'without a first_name' do
       it 'should not save successfully' do
-        @user = User.new(first_name: nil, last_name: 'bobby', email: 'bob@example.com', password: 'password')
+        @user = User.new(first_name: nil, last_name: 'bobby', email: 'bob@example.com', password: 'password', password_confirmation: 'password')
+        @user.save
         expect(@user).to_not be_valid
       end
     end
 
     context 'without a password' do
       it 'should not save successfully' do
-        @user = User.new(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: nil)
+        @user = User.new(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: nil, password_confirmation: 'password')
+        @user.save
         expect(@user).to_not be_valid
       end
     end
 
     context 'with a password of less than 6 characters' do
       it 'should not save successfully' do
-        @user = User.new(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'pw')
+        @user = User.new(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'pw', password_confirmation: 'password')
         expect(@user).to_not be_valid
       end
     end
@@ -72,7 +79,7 @@ RSpec.describe User, type: :model do
 
     context 'with a correct email and password' do
       it 'should return the user object' do
-        @user = User.create!(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password')
+        @user = User.create!(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password', password_confirmation: 'password')
         user = User.authenticate_with_credentials('bob@example.com', 'password')
         expect(user).to eq(@user)
       end
@@ -88,7 +95,7 @@ RSpec.describe User, type: :model do
     context 'with an email that has whitespace' do
       context 'before' do
         it 'should log in successfully' do
-          @user = User.create(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password')
+          @user = User.create!(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password', password_confirmation: 'password')
           user = User.authenticate_with_credentials('   bob@example.com', 'password')
           expect(user).to eq(@user)
         end
@@ -96,7 +103,7 @@ RSpec.describe User, type: :model do
       
       context 'after' do        
         it 'should log in successfully' do
-          @user = User.create(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password')
+          @user = User.create!(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password', password_confirmation: 'password')
           user = User.authenticate_with_credentials('bob@example.com   ', 'password')
           expect(user).to eq(@user)
         end
@@ -105,7 +112,7 @@ RSpec.describe User, type: :model do
 
     context 'with an email that has a different case than in the db' do
       it 'should log in successfully' do
-        @user = User.create(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password')
+        @user = User.create!(first_name: 'bob', last_name: 'bobby', email: 'bob@example.com', password: 'password', password_confirmation: 'password')
         user = User.authenticate_with_credentials('BOB@EXAMPLE.COM', 'password')
         expect(user).to eq(@user)
       end
